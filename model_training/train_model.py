66270f1293c8f99fafae90848e167fffd1559979
Jesse Montgomery <u0002613@rubi.chpc.utah.edu>
2019-08-31 19:09:28 -0600

    model fit changes for rubi

--- model_training/train_model.py
+++ model_training/train_model.py
@@ -37,24 +37,6 @@ def fit_model(input_info):
     model.plot_fit(save_fig=True, outdir=config['Options']['OutputDir'])
 
 
-# with open('../data/community_standard_panel.json') as panel_file:
-#     panel_orgs = json.load(panel_file)
-# org_taxids = [int(key) for key in panel_orgs]
-
-# fqo = fqo.sort_values(['Dilution Factor'])
-# dict_ls = []
-# for idx, row in fqo.iterrows():
-#     dict_ls.append(get_counts(row, concentrations, org_taxids,
-#                    summary_path='../lod_dxsm/community_std_titration'))
-# # Combine list of dictionaries into a single dictionary
-# d = defaultdict(lambda: defaultdict(list))
-# for ls in dict_ls:
-#     for count_dict in ls:
-#         for key in ["Read Counts", "Concentration", "Dilution", "Ctrl Counts"]:
-#             d[count_dict['taxid']][key].append(count_dict[key])
-# # with open(f'model_training/community_std_counts.json', 'w') as outfile:
-# #     json.dump(d, outfile)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a linear quantification model from a dilution series.')
     parser.add_argument('input_file', help='path to the input yaml file.')
