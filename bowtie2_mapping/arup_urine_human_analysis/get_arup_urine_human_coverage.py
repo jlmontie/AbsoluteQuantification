4e9710014b61b3f1f625959286f532d6a36807f1
Jesse Montgomery <u0002613@rubi.chpc.utah.edu>
2019-09-13 21:59:14 -0600

    quitting human coverage analysis on rubi and moving to gcloud

--- bowtie2_mapping/arup_urine_human_analysis/get_arup_urine_human_coverage.py
+++ bowtie2_mapping/arup_urine_human_analysis/get_arup_urine_human_coverage.py
@@ -1,9 +1,14 @@
 import sys
 import pandas as pd
-sys.path.insert(0,'../')
-import pipeline
+# sys.path.insert(0,'../')
+# import pipeline
+import subprocess
 
 index_path = 'index_files'
-fqo = pd.read_csv('../../arup_urine_samples_ge_study/ge_distribution/FastQataloguer_ARUP_Urine_2019-09-11.csv')
-fasta_path = fqo['Original fastq path'].values[0]
-pipeline.main(index_path, fasta_path)
+fqo_path = '/uufs/chpc.utah.edu/common/home/u0002613/AbsoluteQuantification/AbsoluteQuantification/arup_urine_samples_ge_study/ge_distribution/FastQataloguer_ARUP_Urine_2019-09-11.csv'
+fqo = pd.read_csv(fqo_path)
+fasta_path = fqo['Original fastq path']
+for path in fasta_path:
+    subprocess.call(f"cp {path} /scratch/jmontgomery/arup_urine", shell=True)
+# fasta_path = fqo['Original fastq path'].values[0]
+# pipeline.main(index_path, fasta_path)
