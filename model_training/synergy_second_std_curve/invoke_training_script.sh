5f34ebe9ed1255188421638bfebb3fdfe5005ca3
Jesse Montgomery <u0002613@rubi.chpc.utah.edu>
2019-09-23 14:52:30 -0600

    add other orphan files from last commit

---
+++ model_training/synergy_second_std_curve/invoke_training_script.sh
@@ -0,0 +1,3 @@
+TRAIN_SCRIPT=/uufs/chpc.utah.edu/common/home/u0002613/AbsoluteQuantification/AbsoluteQuantification/model_training/train_model.py
+CONFIG=/uufs/chpc.utah.edu/common/home/u0002613/AbsoluteQuantification/AbsoluteQuantification/model_training/synergy_second_std_curve/config_half_log_no_ecoli.yaml
+python $TRAIN_SCRIPT $CONFIG
